dd355427adc876c7aa9812829046ce4acc65a9a0
Anatoli Shykhantsou <39853569+Megape4enka@users.noreply.github.com>
2021-06-04 00:40:28 +0300

    Framewotk start (#4)
    * Finish base framework functional
    
    * comment debug
    
    * Test Webstorm UI for Git

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,37 @@
-export class DomListener {
+import {capitalize} from "./utils";
 
+export class DomListener {
+    constructor($root, listeners = []) {
+        if (!$root) {
+            throw new Error(`No $root provided for DomListener!`)
+        }
+        this.$root = $root
+        this.listeners = listeners
+    }
+
+    initDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            const name = this.name || ''
+            if (!this[method]) {
+                throw new Error(
+                    `Method ${method} is not implemented in ${name} Component`
+                )
+            }
+            this[method] = this[method].bind(this)
+            // Тоже самое что и addEventListener
+            this.$root.on(listener, this[method])
+        })
+    }
+
+    removeDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            this.$root.off(listener, this[method])
+        })
+    }
+}
+// input => onInput
+function getMethodName(eventName) {
+    return 'on' + capitalize(eventName)
 }
